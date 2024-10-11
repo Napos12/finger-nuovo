ee804299bbf542ac176bb35e833dc1d35c40d940
Napos12 <163864922+Napos12@users.noreply.github.com>
2024-10-11 22:20:06 +0200

    Add files via upload

---
+++ MyFinger.h
@@ -0,0 +1,26 @@
+
+
+
+
+typedef struct dati_utente
+{
+    /* data */
+    char user[100];
+    char real_name[100];
+    char directory[100];
+    char shell[100];
+    char tty[20];
+    char login_time[100];
+    char login_time2[100];
+    char office[100];
+    char office_number[100];
+    char home_phone[100];
+    char idle_time[100];
+    char idle_time2[100];
+    char mail[100];
+    char plan[100];
+    char hostname[100];
+}t_dati;
+
+
+
